6854de1bb4887f6c29335d07bebf66e4c155add2
Simon Nodel <snodel@copperleaf.com>
2017-01-30 18:33:30 -0800

    Add initial implementation of mapper

---
+++ src/mapper/mapper.spec.ts
@@ -0,0 +1,77 @@
+import {
+  Mapping
+} from './mapping.decorator';
+import {
+  ajax
+} from 'rxjs/observable/dom/ajax';
+import {
+  Mapper
+} from './mapper';
+
+
+const expectedDecoratorName = 'mapperTestDecoratedName';
+
+function createDecoratedInstance( decorator, constructorToBeDecorated ) {
+  const decoratedConstructor = decorator( constructorToBeDecorated );
+  return new decoratedConstructor();
+}
+
+
+class Foobar {
+  private foo: string;
+  private bar: string;
+
+  constructor( foo = 'fu', bar = 'bar' ) {
+    this.foo = foo;
+    this.bar = bar;
+  }
+
+  getFoo() {
+    return this.foo;
+  }
+
+  getBar() {
+    return this.bar;
+  }
+}
+
+describe( 'Mapper', () => {
+
+  it( 'should be defined', () => {
+    expect( Mapper ).toBeDefined();
+  } );
+
+
+  describe( 'from', () => {
+
+    it( 'should create an instance when given empty object', () => {
+      const mapping = Mapping( expectedDecoratorName );
+
+      const mappedInstance = Mapper.from < Foobar > ( mapping( Foobar ), {} );
+
+      expect( mappedInstance ).toBeDefined();
+      expect( mappedInstance[ '_mappingMeta' ].name ).toBe( expectedDecoratorName )
+      expect( mappedInstance.getFoo() ).toBe( 'fu' );
+      expect( mappedInstance.getBar() ).toBe( 'bar' );
+      expect( mappedInstance[ '_mappingMeta' ].original ).toBeUndefined();
+    } );
+
+
+    it( 'should create an instance and save source when keepOriginal is defined', () => {
+      const mapping = Mapping( expectedDecoratorName, {
+        keepOriginal: true
+      } );
+      const source = {
+        sourceFoo: 'source_foo'
+      };
+
+      const mappedInstance = Mapper.from < Foobar > ( mapping( Foobar ), source );
+
+      expect( mappedInstance ).toBeDefined();
+      expect( mappedInstance[ '_mappingMeta' ].name ).toBe( expectedDecoratorName );
+      expect( mappedInstance[ '_mappingMeta' ].original ).toBe( source );
+    } );
+
+  } );
+
+} );
---
+++ src/mapper/mapper.ts
@@ -0,0 +1,13 @@
+export class Mapper {
+
+  static from<T>(mappingInfo: any, source: any = {}, ...params ): T {
+    const instance = new mappingInfo( ...params );
+
+    if(instance._mappingMeta.options.keepOriginal) {
+      instance._mappingMeta.original = source;
+    }
+
+    return <T> instance;
+  }
+
+}
